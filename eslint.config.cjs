c5fc91ec1c8bac362880534dee808b1e319fd2ce
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2024-08-24 14:53:10 +0200

    feat: Update ESlint to 9.x

---
+++ eslint.config.cjs
@@ -0,0 +1,60 @@
+const globals = require("globals");
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+module.exports = [{
+    ignores: [
+        ".dev-server/**",
+        "**/.prettierrc.js",
+        "**/.eslintrc.js",
+        "**/node_modules/",
+        "**/dist/",
+        "**/coverage/",
+        "**/test/",
+    ],
+}, ...compat.extends("eslint:recommended"), {
+    plugins: {},
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+            ...globals.mocha,
+        },
+
+        ecmaVersion: "latest",
+        sourceType: "commonjs",
+    },
+
+    rules: {
+        indent: ["error", 4, {
+            SwitchCase: 1,
+        }],
+
+        "no-console": "off",
+
+        "no-unused-vars": ["error", {
+            ignoreRestSiblings: true,
+            argsIgnorePattern: "^_",
+        }],
+
+        "no-var": "error",
+        "no-trailing-spaces": "error",
+        "prefer-const": "error",
+
+        quotes: ["error", "single", {
+            avoidEscape: true,
+            allowTemplateLiterals: true,
+        }],
+
+        semi: ["error", "always"],
+    },
+}];
