8f1ab34c1c1db8d8999f83682632792c79aa9b5b
jpawlowski <jpawlowski@users.noreply.github.com>
2026-04-01 14:43:20 +0000

    fix: remove devDeps included in @iobroker/testing (W0063)
    - Remove @types/mocha, @types/sinon, @types/sinon-chai, sinon-chai from
      devDependencies since they are bundled with @iobroker/testing
    - Resolve sinon-chai in test/mocha.setup.js via @iobroker/testing's path
      to avoid hoisting issues with version differences

--- test/mocha.setup.js
+++ test/mocha.setup.js
@@ -6,7 +6,9 @@ process.on('unhandledRejection', (e) => {
 // enable the should interface with sinon
 // and load chai-as-promised and sinon-chai by default
 // Use .default to handle ESM modules loaded via require()
-const _sinonChai = require('sinon-chai');
+// Resolve from @iobroker/testing since these packages are bundled there
+const _testingDir = require('path').dirname(require.resolve('@iobroker/testing/package.json'));
+const _sinonChai = require(require.resolve('sinon-chai', { paths: [_testingDir] }));
 const sinonChai = _sinonChai.default ?? _sinonChai;
 const _chaiAsPromised = require('chai-as-promised');
 const chaiAsPromised = _chaiAsPromised.default ?? _chaiAsPromised;
